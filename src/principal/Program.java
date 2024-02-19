19c8f3032bacc853008cbe350ffb35fe5606247a
LucasMMF <mateusmidao@gmail.com>
2024-02-19 00:51:22 -0300

    feat: altering overload on PrintWriter constructor
    alters the overloaded constructor used on
    PrintWriter, by passing FileOutputStream, so that
    instead of just saving over the pre-existing data
    on the file, the PrintWriter will, instead,
    save on a new line, retaining the previous data
    that was already on the file.

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -8,52 +8,51 @@ import repositories.FuncionarioRepository;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		// syso + CTRL + espaço
 		System.out.println("\n *** CADASTRO DE FUNCIONÁRIO *** \n");
-		
+
 		Funcionario funcionario = new Funcionario();
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Entre com o id do funcionário.....: ");
 		funcionario.setIdFuncionario(Integer.parseInt(scanner.nextLine()));
-		
+
 		System.out.print("Entre com o nome do funcionário...: ");
 		funcionario.setNome(scanner.nextLine());
-		
+
 		System.out.print("Entre com a matrícula.............: ");
 		funcionario.setMatricula(scanner.nextLine());
-		
+
 		System.out.print("Entre com o cpf...................: ");
 		funcionario.setCpf(scanner.nextLine());
-		
+
 		scanner.close();
-		
+
 		try {
-			
+
 			// Criando um objeto para a classe FuncionarioRepository
 			FuncionarioRepository funcionarioRepository = new FuncionarioRepository();
-			
+
 			// Gravando os dados do funcionário em arquivo
 			funcionarioRepository.exportar(funcionario);
-			
+
 			// Imprimindo...
 			System.out.println("\nFUNCIONÁRIO CADASTRADO COM SUCESSO!\n");
 			System.out.println("\tID........: " + funcionario.getIdFuncionario());
 			System.out.println("\tNOME......: " + funcionario.getNome());
 			System.out.println("\tMATRICULA.: " + funcionario.getMatricula());
 			System.out.println("\tCPF.......: " + funcionario.getCpf());
-		}
-		catch(Exception e) {
-			
+		} catch (Exception e) {
+
 			// Imprimindo...
 			System.out.println("\nFALHA AO CADASTRAR FUNCIONÁRIO!");
-			
+
 			// Imprimir o log do erro
 			e.printStackTrace();
-			
+
 		}
-		
+
 	}
 
 }
--- src/repositories/FuncionarioRepository.java
+++ src/repositories/FuncionarioRepository.java
@@ -1,30 +1,34 @@
 package repositories;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
+
 import entities.Funcionario;
 
 public class FuncionarioRepository {
-	
+
 	// Método para gravar os dados de um funcionário em um arquivo
 	public void exportar(Funcionario funcionario) throws Exception {
-		
+
 		// Criando um arquivo em modo de escrita
-		PrintWriter printWriter = new PrintWriter("D:\\temp\\java\\webdeveloper\\funcionario.txt");
-		
+		PrintWriter printWriter = new PrintWriter(
+				new FileOutputStream(new File("D:\\temp\\java\\webdeveloper\\funcionario.txt"), true));
+
 		// Escrever o conteudo do arquivo
-		printWriter.write("\nDADOS DO FUNCIONÁRIO:");
-		
+		printWriter.write("\n\nDADOS DO FUNCIONÁRIO:");
+
 		printWriter.write("\nID.........: " + funcionario.getIdFuncionario());
 		printWriter.write("\nNOME.......: " + funcionario.getNome());
 		printWriter.write("\nMATRICULA..: " + funcionario.getMatricula());
 		printWriter.write("\nCPF........: " + funcionario.getCpf());
-		
+
 		// Salvar o arquivo
 		printWriter.flush();
-		
+
 		// Fechar o arquivo
 		printWriter.close();
-		
+
 	}
 
 }
