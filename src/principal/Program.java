6440caf9edf6cbea2dc4f897b4fd43ad8778df1a
LucasMMF <mateusmidao@gmail.com>
2024-02-19 00:21:05 -0300

    chore: adding input / output for funcionario
    this commit adds the input and output for the
    data in a funcionario object, directly in
    Program.java.

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -1,10 +1,39 @@
 package principal;
 
+import java.util.Scanner;
+
+import entities.Funcionario;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		// syso + CTRL + espaço
+		System.out.println("\n *** CADASTRO DE FUNCIONÁRIO *** \n");
 		
+		Funcionario funcionario = new Funcionario();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Entre com o id do funcionário.....: ");
+		funcionario.setIdFuncionario(Integer.parseInt(scanner.nextLine()));
+		
+		System.out.print("Entre com o nome do funcionário...: ");
+		funcionario.setNome(scanner.nextLine());
+		
+		System.out.print("Entre com a matrícula.............: ");
+		funcionario.setMatricula(scanner.nextLine());
+		
+		System.out.print("Entre com o cpf...................: ");
+		funcionario.setCpf(scanner.nextLine());
+		
+		scanner.close();
+		
+		// Imprimir as informações do funcionário.
+		System.out.println("\nDADOS DO FUNCIONÁRIO:\n");
+		System.out.println("\tID........: " + funcionario.getIdFuncionario());
+		System.out.println("\tNOME......: " + funcionario.getNome());
+		System.out.println("\tMATRICULA.: " + funcionario.getMatricula());
+		System.out.println("\tCPF.......: " + funcionario.getCpf());
 
 	}
 
