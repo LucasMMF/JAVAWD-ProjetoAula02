bc2675b2719630289872f1a4f7b3049cdeb311d6
LucasMMF <mateusmidao@gmail.com>
2024-02-18 22:05:24 -0300

    feat: adding funcionario entity
    adds funcionario entity as well as the basic
    Program.java structure.

---
+++ src/entities/Funcionario.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Funcionario {
+
+	public Integer idFuncionario;
+	public String nome;
+	public String matricula;
+	public String cpf;
+
+	public Integer getIdFuncionario() {
+		return idFuncionario;
+	}
+
+	public void setIdFuncionario(Integer idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
